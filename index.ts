a2fc20dea872bac8b609fc7d9c679ef524469e33
Mazene <mmazenezerguine@gmail.com>
2023-05-07 18:33:12 +0200

    init database connexion

--- index.ts
+++ index.ts
@@ -1,14 +1,28 @@
-import { Express } from "express";
+import { Express , Request , Response } from "express";
 import * as express from 'express' ;
 import { config } from "dotenv";
+import sequelize from './database/dbConnexion';
 
 const app: Express = express();
 const port: number = 3000 ;
 
 config() ;
-app.listen(port);
+
+// database connexion 
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log('Database connected');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 
+// starting server 
 app.listen(port , () => {
     console.log("app running") ;
 });
+
+app.get('/' , (req: Request , res: Response) => {
+    res.send(`app is listening at port ${port}`);
+})
