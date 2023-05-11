48fabc3fefc51dfe55ba26a6f911e77525ae62d8
LoicZHU <zhu.loic@gmail.com>
2023-05-11 20:28:00 +0200

    refactor: add 'process.env'

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,27 @@
-import { config } from "dotenv";
-config() ;
+require('dotenv').config();
 import express, { Express, Request, Response } from 'express';
 
 import sequelize from './database/dbConnexion';
 
 const app: Express = express();
-const port: number = 3000 ;
-
+const HOST = process.env.SERVER_HOST || 'localhost';
+const PORT = process.env.SERVER_PORT || 3000;
 
 // database connexion
-sequelize.sync({ force: false })
-  .then(() => {
-    console.log('Database connected');
-  })
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  });
-
+sequelize
+	.sync({ force: false })
+	.then(() => {
+		console.log('Database connected');
+	})
+	.catch((err) => {
+		console.error('Unable to connect to the database:', err);
+	});
 
 // starting server
-app.listen(port , () => {
-    console.log("app running") ;
+app.listen(PORT, () => {
+	console.log('app running');
 });
 
-app.get('/' , (req: Request , res: Response) => {
-    res.send(`app is listening at port ${port}`);
-})
+app.get('/', (req: Request, res: Response) => {
+	res.send(`app is listening on ${HOST}:${PORT}`);
+});
