21a1d081f7dab4e330990c5a2e8bad8c6ff7531f
LoicZHU <zhu.loic@gmail.com>
2023-05-15 21:58:54 +0200

    feat(animaux): add migration

---
+++ src/migrations/20230515180508-animaux.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable('animaux', {
+			id_animaux: {
+				type: Sequelize.INTEGER,
+				primaryKey: true,
+				autoIncrement: true,
+			},
+			nom: {
+				type: Sequelize.STRING,
+				allowNull: false,
+				unique: true,
+				validate: {
+					notEmpty: true,
+					len: [1, 100],
+				},
+			},
+			sexe: {
+				type: Sequelize.BOOLEAN,
+				allowNull: false,
+			},
+			date_de_naissance: {
+				type: Sequelize.DATE,
+				allowNull: true,
+			},
+		});
+	},
+
+	async down(queryInterface, Sequelize) {
+		await queryInterface.dropTable('animaux');
+	},
+};
