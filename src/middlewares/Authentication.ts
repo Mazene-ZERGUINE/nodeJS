e641fa68b2a4c74cae29d2aea4b887c4db8c8479
Mazene <mmazenezerguine@gmail.com>
2023-05-18 18:43:21 +0100

    adding checkRoles middelwares

--- src/middlewares/Authentication.ts
+++ src/middlewares/Authentication.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { SessionsModel } from '../models/sessions.model';
+import { Roles } from '../models/roles.enum';
 
 export async function isAuthenticated(req: Request, res: Response, next: NextFunction): Promise<void> {
 	try {
@@ -47,3 +48,30 @@ export async function isEmploye(req: Request, res: Response, next: NextFunction)
 		res.status(501).send('internal server error').end();
 	}
 }
+
+export const checkUserRole = (requiredRole: Roles) => {
+	return async (req: Request, res: Response, next: NextFunction) => {
+		const userToken: string | undefined = req.headers.authorization?.split(' ')[1];
+
+		try {
+			const user = await SessionsModel.findOne({
+				where: {
+					token: userToken,
+				},
+			});
+
+			const userRole: string | undefined = await user?.getDataValue('account').id_post.nom;
+			if (userRole !== requiredRole) {
+				res.status(403).send({ message: 'forbidden' }).end();
+				return;
+			}
+
+			next();
+		} catch (error) {
+			res
+				.status(501)
+				.send('internal server error' + error)
+				.end();
+		}
+	};
+};
