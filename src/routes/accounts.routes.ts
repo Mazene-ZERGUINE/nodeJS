43da767d75bc537cb83e4eeca69a7add2e5e3292
Mazene <mmazenezerguine@gmail.com>
2023-05-21 13:03:53 +0100

    creating manegement routes

--- src/routes/accounts.routes.ts
+++ src/routes/accounts.routes.ts
@@ -16,7 +16,7 @@ export default class AccountsRoutes {
 	setRoutes(): void {
 		this.router.get('/', [isAuthenticated, isEmploye], this.accountsController.getAll);
 		this.router.get('/:account_id', [isAuthenticated, isEmploye], this.accountsController.getOne);
-		this.router.post('/create_account', [isAuthenticated, isEmploye], this.accountsController.create);
+		this.router.post('/create_account', [isAuthenticated], this.accountsController.create);
 		this.router.delete('/delete_account/:account_id', [isAuthenticated, isEmploye], this.accountsController.delete);
 		this.router.patch('/update_account/:account_id', [isAuthenticated, isEmploye], this.accountsController.update);
 		this.router.post('/login', this.accountsController.logIn);
