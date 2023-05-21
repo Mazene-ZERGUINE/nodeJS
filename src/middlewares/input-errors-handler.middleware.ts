5fd7ea047b36fefd4b4420a5cd267eeb668ae066
Mazene <mmazenezerguine@gmail.com>
2023-05-21 13:01:44 +0100

    adding body checking middelware

--- src/middlewares/input-errors-handler.middleware.ts
+++ src/middlewares/input-errors-handler.middleware.ts
@@ -1,3 +1,4 @@
+import { isEmpty } from 'lodash';
 import { NextFunction, Request, Response } from 'express';
 import { validationResult } from 'express-validator';
 
@@ -8,5 +9,22 @@ export function handleInputErrors(req: Request, res: Response, next: NextFunctio
 		return;
 	}
 
+	next();
+}
+
+export function emptyBodyError(req: Request, res: Response, next: NextFunction): void {
+	if (req.method === 'POST' || req.method === 'PUT' || req.method === 'PATCH') {
+		if (isEmpty(req.body)) {
+			res.status(400).send({ message: 'bad request' });
+			return;
+		}
+	}
+
+	if (req.method === 'DELETE' || req.method === 'PATCH' || req.method === 'PUT') {
+		if (isEmpty(req.params)) {
+			res.status(400).send({ message: 'bad request' });
+			return;
+		}
+	}
 	next();
 }
