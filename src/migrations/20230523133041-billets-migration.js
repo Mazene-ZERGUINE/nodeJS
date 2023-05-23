63595c70c21eeda4ca384da627935c6053c0bd1a
Mazene <mmazenezerguine@gmail.com>
2023-05-23 17:47:55 +0100

    ticket model and migration

---
+++ src/migrations/20230523133041-billets-migration.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const { INTEGER } = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable(
+			'billets',
+			{
+				ticket_id: {
+					type: Sequelize.INTEGER,
+					primaryKey: true,
+					autoIncrement: true,
+				},
+
+				is_used: {
+					type: Sequelize.BOOLEAN,
+					defaultValue: false,
+				},
+				allowed_spaces: {
+					type: Sequelize.ARRAY(INTEGER),
+					allowNull: false,
+				},
+				date: {
+					type: Sequelize.DATE,
+					allowNull: true,
+				},
+				pass_id: {
+					type: Sequelize.INTEGER,
+					allowNull: false,
+				},
+				current_space: {
+					type: Sequelize.INTEGER,
+					defaultValue: null,
+				},
+				isEnterd: {
+					type: Sequelize.BOOLEAN,
+					defaultValue: false,
+				},
+
+				isExited: {
+					type: Sequelize.BOOLEAN,
+					defaultValue: false,
+				},
+			},
+			{
+				createdAt: false,
+				updatedAt: false,
+				timestamps: false,
+			},
+		);
+	},
+
+	async down(queryInterface, Sequelize) {
+		await queryInterface.dropTable('billets');
+	},
+};
