e51652482dd1c8eaeab076a3bfa022b5e5a966fd
Mazene <mmazenezerguine@gmail.com>
2023-05-24 22:49:56 +0100

    inserting passes in database

--- src/utils/insertRols.ts
+++ src/utils/insertRols.ts
@@ -1,27 +1,45 @@
-import { PostModel } from '../models/post.model';
-import { Roles } from '../models/roles.enum';
+import { Pass, PassModel } from './../models/pass.model';
+import { Passes } from '../models/pass.model';
+import { Post, PostModel } from '../models/post.model';
 
 export async function insertPostes(): Promise<void> {
-	const posts: Roles[] = [
-		Roles.ADMIN,
-		Roles.VISITOR,
-		Roles.VET,
-		Roles.DESK,
-		Roles.CAREARE,
-		Roles.MANTAINER,
-		Roles.SELLER,
-	];
+	const postArray: string[] = ['ADMIN', 'VISITEUR', 'VETERINAIRE', 'ACCUEIL', 'SOINGEUR', 'ENTRETIEN', 'VENDEUR'];
+
+	const check = await PostModel.findAll();
+	if (check.length > 0) {
+		return;
+	}
 
 	try {
-		const postesExist: boolean = (await PostModel.findAll()).length > 0;
-		if (postesExist) {
-			return;
-		}
-
-		for (const post of posts) {
-			await PostModel.create({ nom: post });
-		}
+		postArray.forEach(async (post: string) => {
+			const newPost = await PostModel.create({
+				nom: post,
+			});
+			console.log(newPost.toJSON());
+		});
 	} catch (error) {
 		console.log('error ocuried while inserting roles : ', error);
+	}
+}
+
+export async function insertPasses(): Promise<void> {
+	const passses: string[] = [Passes.DAYPASS, Passes.MONTHPASS, Passes.WEEKEDNPASS, Passes.YEARPASS];
+
+	const checkigng = await PassModel.findAll();
+	if (checkigng.length > 0) {
+		return;
+	}
+
+	try {
+		passses.forEach(async (pass: string) => {
+			console.log(pass);
+			const createPass = await PassModel.create({
+				nom: pass,
+			});
+			console.log(createPass);
+		});
+		console.log('passes loaded');
+	} catch (error) {
+		console.log('error while loading passes');
 	}
 }
