38393e303f8cd00555424ac7253a10c7b733022f
Mazene <mmazenezerguine@gmail.com>
2023-05-25 15:46:07 +0100

    fixing session migration

--- src/migrations/20230517164203-sessions-migration.js
+++ src/migrations/20230517164203-sessions-migration.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	up: async (queryInterface, Sequelize) => {
-		await queryInterface.createTable('posts', {
+		await queryInterface.createTable('sessions', {
 			token: {
 				type: Sequelize.TEXT,
 				unique: true,
@@ -15,6 +15,6 @@ module.exports = {
 	},
 
 	down: async (queryInterface, Sequelize) => {
-		await queryInterface.dropTable('posts');
+		await queryInterface.dropTable('sessions');
 	},
 };
