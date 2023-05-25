60d475f21457d66bf609af85549fa73daf7b94cd
LoicZHU <zhu.loic@gmail.com>
2023-05-25 11:34:39 +0200

    feat(accounts): update migration

--- src/migrations/20230512111118-accounts-migration.js
+++ src/migrations/20230512111118-accounts-migration.js
@@ -1,46 +1,50 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('comptes', {
-      id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-      },
-      nom: {
-        type: Sequelize.STRING
-      },
-      prenom: {
-        type: Sequelize.STRING
-      },
-      email: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      mot_de_pass: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      a_badge: {
-        type: Sequelize.BOOLEAN
-      },
-      est_admin: {
-        type: Sequelize.BOOLEAN
-      },
-      est_employee: {
-        type: Sequelize.BOOLEAN
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable('comptes', {
+			id: {
+				type: Sequelize.INTEGER,
+				autoIncrement: true,
+				primaryKey: true,
+			},
+			nom: {
+				type: Sequelize.STRING,
+			},
+			prenom: {
+				type: Sequelize.STRING,
+			},
+			email: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			mot_de_pass: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			a_badge: {
+				type: Sequelize.BOOLEAN,
+			},
+			est_admin: {
+				type: Sequelize.BOOLEAN,
+			},
+			est_employee: {
+				type: Sequelize.BOOLEAN,
+			},
+			id_post: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		});
+	},
 
 	down: async (queryInterface, Sequelize) => {
 		await queryInterface.dropTable('comptes');
