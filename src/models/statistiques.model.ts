c169c549b55d5ab33bcff13427a93637d57ad251
Mazene <mmazenezerguine@gmail.com>
2023-05-27 19:35:13 +0100

    adding stats migration and model

---
+++ src/models/statistiques.model.ts
@@ -0,0 +1,39 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../database/dbConnexion';
+import { Ticket } from './ticket.model';
+
+export class Statistiques {
+	id!: number;
+	date!: Date;
+	ticket!: Ticket;
+}
+
+export const StatistiquesModel = sequelize.define(
+	'statestiques',
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+
+		date: {
+			type: DataTypes.DATE,
+			allowNull: false,
+		},
+
+		ticket_id: {
+			type: DataTypes.JSONB,
+			allowNull: false,
+		},
+		espace_id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+	},
+	{
+		createdAt: false,
+		updatedAt: false,
+		timestamps: false,
+	},
+);
